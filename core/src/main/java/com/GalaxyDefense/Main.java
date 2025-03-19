121d97611124258b60d988665f4f15eedf47f338
Pedro Luz <pedro_luz@edu.unifil.br>
2025-03-18 22:39:15 -0300

    Protótipo entregável

--- core/src/main/java/com/GalaxyDefense/Main.java
+++ core/src/main/java/com/GalaxyDefense/Main.java
@@ -1,30 +1,36 @@
 package com.GalaxyDefense;
 
-import java.sql.Time;
+import java.util.Iterator;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
-
-import java.util.Iterator;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image, tNave, tBala, tInimigo, tituloPequeno, exit, trofeu, side, score;
+    private Texture image, tNave, tBala, tInimigo, tituloPequeno, exit, trofeu, side, levelImg, scoreImg, telaGameOver;
     private Sprite nave, bala;
     private float posX, posY, velocity, xBala, yBala;
-    private boolean attack;
+    private boolean attack, gameOver;
     private Array<Rectangle> inimigos;
     private long tempoUltimoInimigo;
+    private int score, power, numInimigos, level; 
+
+    private FreeTypeFontGenerator generator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter parameter, parameterLevel, parameterHighScore, parameterScoreAtual;
+    private BitmapFont bitmap, bitmapLevel, bitmapHighScore, bitmapScoreAtual;
 
     @Override
     public void create() {
@@ -34,7 +40,7 @@ public class Main extends ApplicationAdapter {
         nave = new Sprite(tNave); 
         posX = 720 - 190;
         posY = 50;
-        velocity = 10;
+        velocity = 7;
         
         tBala = new Texture("bala.png");
         bala = new Sprite(tBala);
@@ -45,13 +51,51 @@ public class Main extends ApplicationAdapter {
         tInimigo = new Texture("inimigo.png");
         inimigos = new Array<Rectangle>();
         tempoUltimoInimigo = 0;
+
+        score = 0;
+        power = 1;
+        numInimigos = 799999999;
+        level = 1;
+
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 48;
+        parameter.borderWidth = 1;
+        parameter.borderColor = Color.BLACK;
+        parameter.color = new Color(255f / 255f, 204f / 255f, 0f / 255f, 1f); // FFCC00 (Amarelo)
+        bitmap = generator.generateFont(parameter);
+
+        parameterLevel = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 58;
+        parameter.borderWidth = 1;
+        parameter.borderColor = Color.BLACK;
+        parameter.color = Color.WHITE;
+        bitmapLevel = generator.generateFont(parameter);
+
+        parameterHighScore = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 58;
+        parameter.borderWidth = 1;
+        parameter.borderColor = Color.BLACK;
+        parameter.color = new Color(225f / 255f, 255f / 255f, 0f / 255f, 1f);
+        bitmapHighScore = generator.generateFont(parameter);
+
+        parameterScoreAtual = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 156;
+        parameter.borderWidth = 1;
+        parameter.borderColor = Color.BLACK;
+        parameter.color = new Color(255f / 255f, 31f / 255f, 31f / 255f, 1f);
+        bitmapScoreAtual = generator.generateFont(parameter);
+
+        gameOver = false;
         
         //Adições próprias
         tituloPequeno = new Texture("tituloPequeno.png");
         exit = new Texture("exit.png");
         trofeu = new Texture("trofeu.png");
         side = new Texture("side.png");
-        score = new Texture("score.png");
+        levelImg = new Texture("levelImg.png");
+        scoreImg = new Texture("scoreImg.png");
+        telaGameOver = new Texture("tela_gameover.png");
 
     }
 
@@ -64,22 +108,53 @@ public class Main extends ApplicationAdapter {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
         batch.draw(image, 0, 0);
-        if(attack){
-            batch.draw(bala, xBala + nave.getWidth() / 2 - 16, yBala + nave.getHeight() / 2 - 17);
-        }
-        batch.draw(nave, posX, posY);
 
-        for(Rectangle inimigo : inimigos) {
-            batch.draw(tInimigo, inimigo.x, inimigo.y);
-        }
-        
         //Adições próprias
         batch.draw(tituloPequeno, 1113, 464);
         batch.draw(exit, 1167, 247);
         batch.draw(trofeu, 1318, 897);        
         batch.draw(side, 0, 0);        
         batch.draw(side, 1090, 0);        
-        batch.draw(score, 1160, 650);        
+        batch.draw(levelImg, 35, 650);        
+        batch.draw(scoreImg, 1160, 650); 
+
+        if(!gameOver) {
+            if(attack){
+                batch.draw(bala, xBala + nave.getWidth() / 2 - 16, yBala + nave.getHeight() / 2 - 17);
+            }
+            batch.draw(nave, posX, posY);
+
+            for(Rectangle inimigo : inimigos) {
+                batch.draw(tInimigo, inimigo.x, inimigo.y);
+            }
+            bitmap.draw(batch, "" + score, 1240, 735);
+            bitmap.draw(batch, "" + level, 130, 735);
+        } else {
+            batch.draw(telaGameOver, 0, 0);
+            // Inimigos destruídos
+            bitmapLevel.draw(batch, "" + score, 1220, 685);
+            
+            // Level
+            bitmapLevel.draw(batch, "" + level, 1220, 585);
+            
+            // High Score
+            bitmapHighScore.draw(batch, "3450", 1220, 500);
+            
+            //Score da partida atual
+            bitmapScoreAtual.draw(batch, "" + score, 550, 390);
+
+            if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+                score = 0;
+                power = 1;
+                posX = 720 - 190;
+                posY = 50;
+                inimigos.clear();
+                gameOver = false;
+                tempoUltimoInimigo = 0;
+            }
+
+        }
+
         batch.end();
     }
 
@@ -140,15 +215,40 @@ public class Main extends ApplicationAdapter {
     }
 
     private void moveInimigos() {
-        if(TimeUtils.nanoTime() - tempoUltimoInimigo > 999999999) {
+        if(TimeUtils.nanoTime() - tempoUltimoInimigo > numInimigos) {
             this.spawnInimigos();
         }
         for(Iterator<Rectangle> iter = inimigos.iterator(); iter.hasNext();) {
             Rectangle inimigo = iter.next();
             inimigo.y -= 400 * Gdx.graphics.getDeltaTime(); 
+            // Colisão com a bala
+            if(colisao(inimigo.x, inimigo.y, inimigo.width, inimigo.height, xBala, yBala, bala.getWidth(), bala.getHeight()) && attack) {
+                ++score;
+                if(score % 15 == 0) {
+                    numInimigos -= 100;
+                    ++level;
+                }
+                attack = false;
+                iter.remove();
+                xBala = posX; 
+                yBala = posY;
+            // Colisão com a nave
+            } else if(colisao(inimigo.x, inimigo.y, inimigo.width, inimigo.height, posX, posY, nave.getWidth(), nave.getHeight()) && !gameOver) {
+                --power;
+                if(power <= 0) {
+                    gameOver = true;
+                }
+                iter.remove();
+            }
             if(inimigo.y < 0) {
                 iter.remove();
             }
         }
+    }
+
+    private boolean colisao(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
+        if(x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2) {
+            return true;
+        } return false;
     }
 }
