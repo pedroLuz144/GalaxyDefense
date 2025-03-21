71371f41b474cdb90f7819469eb13f1933301f27
Pedro Luz <p.h.r.s.l123@gmail.com>
2025-03-20 21:06:21 -0300

    Começando implementação com o banco de dados

--- core/src/main/java/com/GalaxyDefense/Main.java
+++ core/src/main/java/com/GalaxyDefense/Main.java
@@ -17,7 +17,10 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image, tNave, tBala, tInimigo, tituloPequeno, exit, trofeu, side, levelImg, scoreImg, telaGameOver;
@@ -26,10 +29,11 @@ public class Main extends ApplicationAdapter {
     private boolean attack, gameOver;
     private Array<Rectangle> inimigos;
     private long tempoUltimoInimigo;
-    private int score, power, numInimigos, level; 
+    private int score, power, numInimigos, level, highScore;
 
     private FreeTypeFontGenerator generator;
-    private FreeTypeFontGenerator.FreeTypeFontParameter parameter, parameterLevel, parameterHighScore, parameterScoreAtual;
+    private FreeTypeFontGenerator.FreeTypeFontParameter parameter, parameterLevel, parameterHighScore,
+            parameterScoreAtual;
     private BitmapFont bitmap, bitmapLevel, bitmapHighScore, bitmapScoreAtual;
 
     @Override
@@ -37,24 +41,24 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         image = new Texture("background.png");
         tNave = new Texture("nave.png");
-        nave = new Sprite(tNave); 
+        nave = new Sprite(tNave);
         posX = 720 - 190;
         posY = 50;
-        velocity = 7;
-        
+        velocity = 10;
+
         tBala = new Texture("bala.png");
         bala = new Sprite(tBala);
         xBala = posX;
         yBala = posY;
         attack = false;
-        
+
         tInimigo = new Texture("inimigo.png");
         inimigos = new Array<Rectangle>();
         tempoUltimoInimigo = 0;
 
         score = 0;
         power = 1;
-        numInimigos = 799999999;
+        numInimigos = 899999999;
         level = 1;
 
         generator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
@@ -87,8 +91,8 @@ public class Main extends ApplicationAdapter {
         bitmapScoreAtual = generator.generateFont(parameter);
 
         gameOver = false;
-        
-        //Adições próprias
+
+        // Adições próprias
         tituloPequeno = new Texture("tituloPequeno.png");
         exit = new Texture("exit.png");
         trofeu = new Texture("trofeu.png");
@@ -109,22 +113,22 @@ public class Main extends ApplicationAdapter {
         batch.begin();
         batch.draw(image, 0, 0);
 
-        //Adições próprias
+        // Adições próprias
         batch.draw(tituloPequeno, 1113, 464);
         batch.draw(exit, 1167, 247);
-        batch.draw(trofeu, 1318, 897);        
-        batch.draw(side, 0, 0);        
-        batch.draw(side, 1090, 0);        
-        batch.draw(levelImg, 35, 650);        
-        batch.draw(scoreImg, 1160, 650); 
+        batch.draw(trofeu, 1318, 897);
+        batch.draw(side, 0, 0);
+        batch.draw(side, 1090, 0);
+        batch.draw(levelImg, 35, 650);
+        batch.draw(scoreImg, 1160, 650);
 
-        if(!gameOver) {
-            if(attack){
+        if (!gameOver) {
+            if (attack) {
                 batch.draw(bala, xBala + nave.getWidth() / 2 - 16, yBala + nave.getHeight() / 2 - 17);
             }
             batch.draw(nave, posX, posY);
 
-            for(Rectangle inimigo : inimigos) {
+            for (Rectangle inimigo : inimigos) {
                 batch.draw(tInimigo, inimigo.x, inimigo.y);
             }
             bitmap.draw(batch, "" + score, 1240, 735);
@@ -133,19 +137,20 @@ public class Main extends ApplicationAdapter {
             batch.draw(telaGameOver, 0, 0);
             // Inimigos destruídos
             bitmapLevel.draw(batch, "" + score, 1220, 685);
-            
+
             // Level
             bitmapLevel.draw(batch, "" + level, 1220, 585);
-            
+
             // High Score
-            bitmapHighScore.draw(batch, "3450", 1220, 500);
-            
-            //Score da partida atual
+            bitmapHighScore.draw(batch, "" + highScore, 1220, 500);
+
+            // Score da partida atual
             bitmapScoreAtual.draw(batch, "" + score, 550, 390);
 
-            if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
                 score = 0;
                 power = 1;
+                level = 1;
                 posX = 720 - 190;
                 posY = 50;
                 inimigos.clear();
@@ -166,41 +171,41 @@ public class Main extends ApplicationAdapter {
     }
 
     private void moveNave() {
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            if(posX < 1440 - 88 - 355) {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            if (posX < 1440 - 88 - 355) {
                 posX += velocity;
             }
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            if(posX > 359) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            if (posX > 359) {
                 posX -= velocity;
             }
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            if(posY < 200) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            if (posY < 200) {
                 posY += velocity;
             }
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            if(posY > 20) {
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            if (posY > 20) {
                 posY -= velocity;
             }
         }
     }
 
     private void moveBala() {
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack) {
             attack = true;
             xBala = posX;
         }
-        
-        if(attack) {
-            if(yBala < Gdx.graphics.getWidth()) {
+
+        if (attack) {
+            if (yBala < Gdx.graphics.getWidth()) {
                 yBala += 15;
             } else {
                 xBala = posX;
                 yBala = posY;
-                attack = false;    
+                attack = false;
             }
         } else {
             xBala = posX;
@@ -215,40 +220,43 @@ public class Main extends ApplicationAdapter {
     }
 
     private void moveInimigos() {
-        if(TimeUtils.nanoTime() - tempoUltimoInimigo > numInimigos) {
+        if (TimeUtils.nanoTime() - tempoUltimoInimigo > numInimigos) {
             this.spawnInimigos();
         }
-        for(Iterator<Rectangle> iter = inimigos.iterator(); iter.hasNext();) {
+        for (Iterator<Rectangle> iter = inimigos.iterator(); iter.hasNext();) {
             Rectangle inimigo = iter.next();
-            inimigo.y -= 400 * Gdx.graphics.getDeltaTime(); 
+            inimigo.y -= 400 * Gdx.graphics.getDeltaTime();
             // Colisão com a bala
-            if(colisao(inimigo.x, inimigo.y, inimigo.width, inimigo.height, xBala, yBala, bala.getWidth(), bala.getHeight()) && attack) {
+            if (colisao(inimigo.x, inimigo.y, inimigo.width, inimigo.height, xBala, yBala, bala.getWidth(),
+                    bala.getHeight()) && attack) {
                 ++score;
-                if(score % 15 == 0) {
+                if (score % 15 == 0) {
                     numInimigos -= 100;
                     ++level;
                 }
                 attack = false;
                 iter.remove();
-                xBala = posX; 
+                xBala = posX;
                 yBala = posY;
-            // Colisão com a nave
-            } else if(colisao(inimigo.x, inimigo.y, inimigo.width, inimigo.height, posX, posY, nave.getWidth(), nave.getHeight()) && !gameOver) {
+                // Colisão com a nave
+            } else if (colisao(inimigo.x, inimigo.y, inimigo.width, inimigo.height, posX, posY, nave.getWidth(),
+                    nave.getHeight()) && !gameOver) {
                 --power;
-                if(power <= 0) {
+                if (power <= 0) {
                     gameOver = true;
                 }
                 iter.remove();
             }
-            if(inimigo.y < 0) {
+            if (inimigo.y < 0) {
                 iter.remove();
             }
         }
     }
 
     private boolean colisao(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
-        if(x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2) {
+        if (x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2) {
             return true;
-        } return false;
+        }
+        return false;
     }
 }
